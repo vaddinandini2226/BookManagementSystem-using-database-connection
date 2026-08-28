7a7522d38d5140b863ea729b3f952d951c8bb00a
vaddinandini2226 <vaddinandini2004@gmail.com>
2026-08-28 12:09:26 +0530

    [Nandini]: Added BookDAO interface

---
+++ src/main/java/com/bridgelabz/DAO/BookDAO.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.DAO;
+
+import com.bridgelabz.model.Book;
+
+import java.util.List;
+
+public interface BookDAO {
+
+    void addBook();
+
+    void updateBook();
+
+    void deleteBook();
+
+    void getAllBooks();
+}
